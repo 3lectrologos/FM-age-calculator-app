48a246673734144dd9cc7b564166e08daec05172
Alkis Gotovos <alkis.gotovos@gmail.com>
2023-11-24 19:14:38 +0100

    Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        purple: 'hsl(259 100% 65%)',
+        lightred: 'hsl(0 100% 67%)',
+        offwhite: 'hsl(0 0% 94%)',
+        lightgray: 'hsl(0, 0% 86%)',
+        smokeygray: 'hsl(0 1% 44%)',
+        offblack: 'hsl(0 0% 8%)'
+      }
     },
   },
   plugins: [],
